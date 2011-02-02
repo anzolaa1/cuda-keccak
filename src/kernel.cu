933628d20a17c03e2055137806ac71c2073ea24c
chindemi.g <chindemi.g@gmail.com>
2011-02-02 17:04:28 +0000

    Unrolling

--- src/kernel.cu
+++ src/kernel.cu
@@ -8,11 +8,11 @@
 //#define __DEBUG_MODE_ON__
 #define __BENCHMARK_MODE_ON__
 
-#define THREADS_PER_BLOCK 256
+#define THREADS_PER_BLOCK 32
 #define ROUNDS_NUMBER 24
 #define WORDS_NUMBER 25
 
-#define index(x, y) (((x)%5)+5*((y)%5))
+//#define index(x, y) (((x)%5)+5*((y)%5))
 
 // NVCC Bug
 //#define ROL64(a, offset) ((offset != 0) ? ((((UINT64)a) << offset) ^ (((UINT64)a) >> (64-offset))) : a)
@@ -40,19 +40,19 @@ __constant__ UINT64 KeccakRoundConstants[ROUNDS_NUMBER];
 __constant__ unsigned int KeccakRhoOffsets[WORDS_NUMBER];
 
 
-/*
- *
- */
+
 __global__ void kernel(UINT64 *messages_d, UINT64 *state_d)
 {
+	
 	int offset = WORDS_NUMBER * (threadIdx.x + blockIdx.x * blockDim.x);
 	unsigned int i, x, y, round_number;
-	UINT64 A[WORDS_NUMBER], tempA[WORDS_NUMBER], C[5], D[5];
+	UINT64 A[25], tempA[25], C[5], D[5];
 	
+
 	// Absorbing
 	for(i = 0; i < WORDS_NUMBER; i++)
         	A[i] = state_d[offset + i] ^ messages_d[offset + i];
-		/*
+	/*
     	for(round_number = 0; round_number < ROUNDS_NUMBER; round_number++) {
 		// Theta
 		for(x=0; x<5; x++) {
@@ -85,11 +85,12 @@ __global__ void kernel(UINT64 *messages_d, UINT64 *state_d)
 		}
 		
         	// Iota
-		A[index(0, 0)] ^= KeccakRoundConstants[round_number];
+		A[0] ^= KeccakRoundConstants[round_number];
     }
-    */
+
     for(i = 0; i < WORDS_NUMBER; i++)
         state_d[offset + i] = A[i];
+*/
 }
 
 
@@ -99,15 +100,41 @@ __global__ void kernel(UINT64 *messages_d, UINT64 *state_d)
 __global__ void kernel_optimixed(UINT64 *messages_d, UINT64 *state_d)
 {
 	int offset = WORDS_NUMBER * (threadIdx.x + blockIdx.x * blockDim.x);
-	unsigned int x, y, round_number;
-	UINT64 A[WORDS_NUMBER], tempA[WORDS_NUMBER], C[5], D[5];
-	
+	unsigned int x;
+	UINT64 A[25], tempA[25], C[5], D[5];
+
 	// Absorbing
+	/*
 	for(x = 0; x < WORDS_NUMBER; x++)
         	A[x] = state_d[offset + x] ^ messages_d[offset + x];
-        	
-		
-    	for(round_number = 0; round_number < ROUNDS_NUMBER; round_number++) {
+	*/
+	A[0] = state_d[offset + 0] ^ messages_d[offset + 0];
+	A[1] = state_d[offset + 1] ^ messages_d[offset + 1];
+	A[2] = state_d[offset + 2] ^ messages_d[offset + 2];
+	A[3] = state_d[offset + 3] ^ messages_d[offset + 3];
+	A[4] = state_d[offset + 4] ^ messages_d[offset + 4];
+	A[5] = state_d[offset + 5] ^ messages_d[offset + 5];
+	A[6] = state_d[offset + 6] ^ messages_d[offset + 6];
+	A[7] = state_d[offset + 7] ^ messages_d[offset + 7];
+	A[8] = state_d[offset + 8] ^ messages_d[offset + 8];
+	A[9] = state_d[offset + 9] ^ messages_d[offset + 9];
+	A[10] = state_d[offset + 10] ^ messages_d[offset + 10];
+	A[11] = state_d[offset + 11] ^ messages_d[offset + 11];
+	A[12] = state_d[offset + 12] ^ messages_d[offset + 12];
+	A[13] = state_d[offset + 13] ^ messages_d[offset + 13];
+	A[14] = state_d[offset + 14] ^ messages_d[offset + 14];
+	A[15] = state_d[offset + 15] ^ messages_d[offset + 15];
+	A[16] = state_d[offset + 16] ^ messages_d[offset + 16];
+	A[17] = state_d[offset + 17] ^ messages_d[offset + 17];
+	A[18] = state_d[offset + 18] ^ messages_d[offset + 18];
+	A[19] = state_d[offset + 19] ^ messages_d[offset + 19];
+	A[20] = state_d[offset + 20] ^ messages_d[offset + 20];
+	A[21] = state_d[offset + 21] ^ messages_d[offset + 21];
+	A[22] = state_d[offset + 22] ^ messages_d[offset + 22];
+	A[23] = state_d[offset + 23] ^ messages_d[offset + 23];
+	A[24] = state_d[offset + 24] ^ messages_d[offset + 24];
+
+    	for(x = 0; x < ROUNDS_NUMBER; x++) {
 		// Theta
 		/*for(x=0; x<5; x++) {
 			C[x] = 0; 
@@ -168,29 +195,29 @@ __global__ void kernel_optimixed(UINT64 *messages_d, UINT64 *state_d)
 		A[15] = A[15] ^ D[1] ^ C[4];
 		A[20] = A[20] ^ D[1] ^ C[4];
 		
-		A[index(1, 0)] = A[index(1,0)] ^ D[2] ^ C[0];
-		A[index(1, 1)] = A[index(1,1)] ^ D[2] ^ C[0];
-		A[index(1, 2)] = A[index(1,2)] ^ D[2] ^ C[0];
-		A[index(1, 3)] = A[index(1,3)] ^ D[2] ^ C[0];
-		A[index(1, 4)] = A[index(1,4)] ^ D[2] ^ C[0];
+		A[1] = A[1] ^ D[2] ^ C[0];
+		A[6] = A[6] ^ D[2] ^ C[0];
+		A[11] = A[11] ^ D[2] ^ C[0];
+		A[16] = A[16] ^ D[2] ^ C[0];
+		A[21] = A[21] ^ D[2] ^ C[0];
 		
-		A[index(2, 0)] = A[index(2,0)] ^ D[3] ^ C[1];
-		A[index(2, 1)] = A[index(2,1)] ^ D[3] ^ C[1];
-		A[index(2, 2)] = A[index(2,2)] ^ D[3] ^ C[1];
-		A[index(2, 3)] = A[index(2,3)] ^ D[3] ^ C[1];
-		A[index(2, 4)] = A[index(2,4)] ^ D[3] ^ C[1];
+		A[2] = A[2] ^ D[3] ^ C[1];
+		A[7] = A[7] ^ D[3] ^ C[1];
+		A[12] = A[12] ^ D[3] ^ C[1];
+		A[17] = A[17] ^ D[3] ^ C[1];
+		A[22] = A[22] ^ D[3] ^ C[1];
 		
-		A[index(3, 0)] = A[index(3,0)] ^ D[4] ^ C[2];
-		A[index(3, 1)] = A[index(3,1)] ^ D[4] ^ C[2];
-		A[index(3, 2)] = A[index(3,2)] ^ D[4] ^ C[2];
-		A[index(3, 3)] = A[index(3,3)] ^ D[4] ^ C[2];
-		A[index(3, 4)] = A[index(3,4)] ^ D[4] ^ C[2];
+		A[3] = A[3] ^ D[4] ^ C[2];
+		A[8] = A[8] ^ D[4] ^ C[2];
+		A[13] = A[13] ^ D[4] ^ C[2];
+		A[18] = A[18] ^ D[4] ^ C[2];
+		A[23] = A[23] ^ D[4] ^ C[2];
 		
-		A[index(4, 0)] = A[index(4,0)] ^ D[0] ^ C[3];
-		A[index(4, 1)] = A[index(4,1)] ^ D[0] ^ C[3];
-		A[index(4, 2)] = A[index(4,2)] ^ D[0] ^ C[3];
-		A[index(4, 3)] = A[index(4,3)] ^ D[0] ^ C[3];
-		A[index(4, 4)] = A[index(4,4)] ^ D[0] ^ C[3];
+		A[4] = A[4] ^ D[0] ^ C[3];
+		A[9] = A[9] ^ D[0] ^ C[3];
+		A[14] = A[14] ^ D[0] ^ C[3];
+		A[19] = A[19] ^ D[0] ^ C[3];
+		A[24] = A[24] ^ D[0] ^ C[3];
         	// Rho
 		/*for(x=0; x<5; x++) 
 			for(y=0; y<5; y++)
@@ -199,35 +226,35 @@ __global__ void kernel_optimixed(UINT64 *messages_d, UINT64 *state_d)
 		
 		
 		//RHO UNROLLED		
-		A[index(0, 0)] = ROL64(A[index(0, 0)], KeccakRhoOffsets[index(0, 0)]);
-		A[index(0, 1)] = ROL64(A[index(0, 1)], KeccakRhoOffsets[index(0, 1)]);
-		A[index(0, 2)] = ROL64(A[index(0, 2)], KeccakRhoOffsets[index(0, 2)]);
-		A[index(0, 3)] = ROL64(A[index(0, 3)], KeccakRhoOffsets[index(0, 3)]);
-		A[index(0, 4)] = ROL64(A[index(0, 4)], KeccakRhoOffsets[index(0, 4)]);
+		A[0] = ROL64(A[0], KeccakRhoOffsets[0]);
+		A[5] = ROL64(A[5], KeccakRhoOffsets[5]);
+		A[10] = ROL64(A[10], KeccakRhoOffsets[10]);
+		A[15] = ROL64(A[15], KeccakRhoOffsets[15]);
+		A[20] = ROL64(A[20], KeccakRhoOffsets[20]);
 		
-		A[index(1, 0)] = ROL64(A[index(1, 0)], KeccakRhoOffsets[index(1, 0)]);
-		A[index(1, 1)] = ROL64(A[index(1, 1)], KeccakRhoOffsets[index(1, 1)]);
-		A[index(1, 2)] = ROL64(A[index(1, 2)], KeccakRhoOffsets[index(1, 2)]);
-		A[index(1, 3)] = ROL64(A[index(1, 3)], KeccakRhoOffsets[index(1, 3)]);
-		A[index(1, 4)] = ROL64(A[index(1, 4)], KeccakRhoOffsets[index(1, 4)]);
+		A[1] = ROL64(A[1], KeccakRhoOffsets[1]);
+		A[6] = ROL64(A[6], KeccakRhoOffsets[6]);
+		A[11] = ROL64(A[11], KeccakRhoOffsets[11]);
+		A[16] = ROL64(A[16], KeccakRhoOffsets[16]);
+		A[21] = ROL64(A[21], KeccakRhoOffsets[21]);
 
-		A[index(2, 0)] = ROL64(A[index(2, 0)], KeccakRhoOffsets[index(2, 0)]);
-		A[index(2, 1)] = ROL64(A[index(2, 1)], KeccakRhoOffsets[index(2, 1)]);
-		A[index(2, 2)] = ROL64(A[index(2, 2)], KeccakRhoOffsets[index(2, 2)]);
-		A[index(2, 3)] = ROL64(A[index(2, 3)], KeccakRhoOffsets[index(2, 3)]);
-		A[index(2, 4)] = ROL64(A[index(2, 4)], KeccakRhoOffsets[index(2, 4)]);
+		A[2] = ROL64(A[2], KeccakRhoOffsets[2]);
+		A[7] = ROL64(A[7], KeccakRhoOffsets[7]);
+		A[12] = ROL64(A[12], KeccakRhoOffsets[12]);
+		A[17] = ROL64(A[17], KeccakRhoOffsets[17]);
+		A[22] = ROL64(A[22], KeccakRhoOffsets[22]);
 		
-		A[index(3, 0)] = ROL64(A[index(3, 0)], KeccakRhoOffsets[index(3, 0)]);
-		A[index(3, 1)] = ROL64(A[index(3, 1)], KeccakRhoOffsets[index(3, 1)]);
-		A[index(3, 2)] = ROL64(A[index(3, 2)], KeccakRhoOffsets[index(3, 2)]);
-		A[index(3, 3)] = ROL64(A[index(3, 3)], KeccakRhoOffsets[index(3, 3)]);
-		A[index(3, 4)] = ROL64(A[index(3, 4)], KeccakRhoOffsets[index(3, 4)]);
+		A[3] = ROL64(A[3], KeccakRhoOffsets[3]);
+		A[8] = ROL64(A[8], KeccakRhoOffsets[8]);
+		A[13] = ROL64(A[13], KeccakRhoOffsets[13]);
+		A[18] = ROL64(A[18], KeccakRhoOffsets[18]);
+		A[23] = ROL64(A[23], KeccakRhoOffsets[23]);
 		
-		A[index(4, 0)] = ROL64(A[index(4, 0)], KeccakRhoOffsets[index(4, 0)]);
-		A[index(4, 1)] = ROL64(A[index(4, 1)], KeccakRhoOffsets[index(4, 1)]);
-		A[index(4, 2)] = ROL64(A[index(4, 2)], KeccakRhoOffsets[index(4, 2)]);
-		A[index(4, 3)] = ROL64(A[index(4, 3)], KeccakRhoOffsets[index(4, 3)]);
-		A[index(4, 4)] = ROL64(A[index(4, 4)], KeccakRhoOffsets[index(4, 4)]);
+		A[4] = ROL64(A[4], KeccakRhoOffsets[4]);
+		A[9] = ROL64(A[9], KeccakRhoOffsets[9]);
+		A[14] = ROL64(A[14], KeccakRhoOffsets[14]);
+		A[19] = ROL64(A[19], KeccakRhoOffsets[19]);
+		A[24] = ROL64(A[24], KeccakRhoOffsets[24]);
 		
 		/*
 		// Pi
@@ -239,65 +266,65 @@ __global__ void kernel_optimixed(UINT64 *messages_d, UINT64 *state_d)
 				A[index(0*x+1*y, 2*x+3*y)] = tempA[index(x, y)];
 		*/
 		//UNROLLED PI
-		tempA[index(0, 0)] = A[index(0, 0)];
-		tempA[index(0, 1)] = A[index(0, 1)];
-		tempA[index(0, 2)] = A[index(0, 2)];
-		tempA[index(0, 3)] = A[index(0, 3)];
-		tempA[index(0, 4)] = A[index(0, 4)];
+		tempA[0] = A[0];
+		tempA[5] = A[5];
+		tempA[10] = A[10];
+		tempA[15] = A[15];
+		tempA[20] = A[20];
 		
-		tempA[index(1, 0)] = A[index(1, 0)];
-		tempA[index(1, 1)] = A[index(1, 1)];
-		tempA[index(1, 2)] = A[index(1, 2)];
-		tempA[index(1, 3)] = A[index(1, 3)];
-		tempA[index(1, 4)] = A[index(1, 4)];
+		tempA[1] = A[1];
+		tempA[6] = A[6];
+		tempA[11] = A[11];
+		tempA[16] = A[16];
+		tempA[21] = A[21];
 		
-		tempA[index(2, 0)] = A[index(2, 0)];
-		tempA[index(2, 1)] = A[index(2, 1)];
-		tempA[index(2, 2)] = A[index(2, 2)];
-		tempA[index(2, 3)] = A[index(2, 3)];
-		tempA[index(2, 4)] = A[index(2, 4)];
+		tempA[2] = A[2];
+		tempA[7] = A[7];
+		tempA[12] = A[12];
+		tempA[17] = A[17];
+		tempA[22] = A[22];
 		
-		tempA[index(3, 0)] = A[index(3, 0)];
-		tempA[index(3, 1)] = A[index(3, 1)];
-		tempA[index(3, 2)] = A[index(3, 2)];
-		tempA[index(3, 3)] = A[index(3, 3)];
-		tempA[index(3, 4)] = A[index(3, 4)];
+		tempA[3] = A[3];
+		tempA[8] = A[8];
+		tempA[13] = A[13];
+		tempA[18] = A[18];
+		tempA[23] = A[23];
 		
-		tempA[index(4, 0)] = A[index(4, 0)];
-		tempA[index(4, 1)] = A[index(4, 1)];
-		tempA[index(4, 2)] = A[index(4, 2)];
-		tempA[index(4, 3)] = A[index(4, 3)];
-		tempA[index(4, 4)] = A[index(4, 4)];
+		tempA[4] = A[4];
+		tempA[9] = A[9];
+		tempA[14] = A[14];
+		tempA[19] = A[19];
+		tempA[24] = A[24];
 		
-		A[index(0, 2*0+3*0)] = tempA[index(0, 0)];
-		A[index(1, 2*0+3*1)] = tempA[index(0, 1)];
-		A[index(2, 2*0+3*2)] = tempA[index(0, 2)];
-		A[index(3, 2*0+3*3)] = tempA[index(0, 3)];
-		A[index(4, 2*0+3*4)] = tempA[index(0, 4)];
+		A[0] = tempA[0];
+		A[16] = tempA[5];
+		A[7] = tempA[10];
+		A[23] = tempA[15];
+		A[14] = tempA[20];
 		
-		A[index(0, 2*1+3*0)] = tempA[index(1, 0)];
-		A[index(1, 2*1+3*1)] = tempA[index(1, 1)];
-		A[index(2, 2*1+3*2)] = tempA[index(1, 2)];
-		A[index(3, 2*1+3*3)] = tempA[index(1, 3)];
-		A[index(4, 2*1+3*4)] = tempA[index(1, 4)];
+		A[10] = tempA[1];
+		A[1] = tempA[6];
+		A[17] = tempA[11];
+		A[8] = tempA[16];
+		A[24] = tempA[21];
 		
-		A[index(0, 2*2+3*0)] = tempA[index(2, 0)];
-		A[index(1, 2*2+3*1)] = tempA[index(2, 1)];
-		A[index(2, 2*2+3*2)] = tempA[index(2, 2)];
-		A[index(3, 2*2+3*3)] = tempA[index(2, 3)];
-		A[index(4, 2*2+3*4)] = tempA[index(2, 4)];
+		A[20] = tempA[2];
+		A[11] = tempA[7];
+		A[2] = tempA[12];
+		A[18] = tempA[17];
+		A[9] = tempA[22];
 		
-		A[index(0, 2*3+3*0)] = tempA[index(3, 0)];
-		A[index(1, 2*3+3*1)] = tempA[index(3, 1)];
-		A[index(2, 2*3+3*2)] = tempA[index(3, 2)];
-		A[index(3, 2*3+3*3)] = tempA[index(3, 3)];
-		A[index(4, 2*3+3*4)] = tempA[index(3, 4)];
+		A[5] = tempA[3];
+		A[21] = tempA[8];
+		A[12] = tempA[13];
+		A[3] = tempA[18];
+		A[19] = tempA[23];
 		
-		A[index(0, 2*4+3*0)] = tempA[index(4, 0)];
-		A[index(1, 2*4+3*1)] = tempA[index(4, 1)];
-		A[index(2, 2*4+3*2)] = tempA[index(4, 2)];
-		A[index(3, 2*4+3*3)] = tempA[index(4, 3)];
-		A[index(4, 2*4+3*4)] = tempA[index(4, 4)];
+		A[15] = tempA[4];
+		A[6] = tempA[9];
+		A[22] = tempA[14];
+		A[13] = tempA[19];
+		A[4] = tempA[24];
 		
 		
         // Chi
@@ -308,63 +335,63 @@ __global__ void kernel_optimixed(UINT64 *messages_d, UINT64 *state_d)
 				A[index(x, y)] = C[x];
 		}*/
 		
-		C[0] = A[index(0, 0)] ^ ((~A[index(0+1, 0)]) & A[index(0+2, 0)]);
-		C[1] = A[index(1, 0)] ^ ((~A[index(1+1, 0)]) & A[index(1+2, 0)]);
-		C[2] = A[index(2, 0)] ^ ((~A[index(2+1, 0)]) & A[index(2+2, 0)]);
-		C[3] = A[index(3, 0)] ^ ((~A[index(3+1, 0)]) & A[index(3+2, 0)]);
-		C[4] = A[index(4, 0)] ^ ((~A[index(4+1, 0)]) & A[index(4+2, 0)]);
-		A[index(0, 0)] = C[0];
-		A[index(1, 0)] = C[1];
-		A[index(2, 0)] = C[2];
-		A[index(3, 0)] = C[3];
-		A[index(4, 0)] = C[4];
+		C[0] = A[0] ^ ((~A[1]) & A[2]);
+		C[1] = A[1] ^ ((~A[2]) & A[3]);
+		C[2] = A[2] ^ ((~A[3]) & A[4]);
+		C[3] = A[3] ^ ((~A[4]) & A[0]);
+		C[4] = A[4] ^ ((~A[0]) & A[1]);
+		A[0] = C[0];
+		A[1] = C[1];
+		A[2] = C[2];
+		A[3] = C[3];
+		A[4] = C[4];
 		
-		C[0] = A[index(0, 1)] ^ ((~A[index(0+1, 1)]) & A[index(0+2, 1)]);
-		C[1] = A[index(1, 1)] ^ ((~A[index(1+1, 1)]) & A[index(1+2, 1)]);
-		C[2] = A[index(2, 1)] ^ ((~A[index(2+1, 1)]) & A[index(2+2, 1)]);
-		C[3] = A[index(3, 1)] ^ ((~A[index(3+1, 1)]) & A[index(3+2, 1)]);
-		C[4] = A[index(4, 1)] ^ ((~A[index(4+1, 1)]) & A[index(4+2, 1)]);
-		A[index(0, 1)] = C[0];
-		A[index(1, 1)] = C[1];
-		A[index(2, 1)] = C[2];
-		A[index(3, 1)] = C[3];
-		A[index(4, 1)] = C[4];
+		C[0] = A[5] ^ ((~A[6]) & A[7]);
+		C[1] = A[6] ^ ((~A[7]) & A[8]);
+		C[2] = A[7] ^ ((~A[8]) & A[9]);
+		C[3] = A[8] ^ ((~A[9]) & A[5]);
+		C[4] = A[9] ^ ((~A[5]) & A[6]);
+		A[5] = C[0];
+		A[6] = C[1];
+		A[7] = C[2];
+		A[8] = C[3];
+		A[9] = C[4];
 		
-		C[0] = A[index(0, 2)] ^ ((~A[index(0+1, 2)]) & A[index(0+2, 2)]);
-		C[1] = A[index(1, 2)] ^ ((~A[index(1+1, 2)]) & A[index(1+2, 2)]);
-		C[2] = A[index(2, 2)] ^ ((~A[index(2+1, 2)]) & A[index(2+2, 2)]);
-		C[3] = A[index(3, 2)] ^ ((~A[index(3+1, 2)]) & A[index(3+2, 2)]);
-		C[4] = A[index(4, 2)] ^ ((~A[index(4+1, 2)]) & A[index(4+2, 2)]);
-		A[index(0, 2)] = C[0];
-		A[index(1, 2)] = C[1];
-		A[index(2, 2)] = C[2];
-		A[index(3, 2)] = C[3];
-		A[index(4, 2)] = C[4];
+		C[0] = A[10] ^ ((~A[11]) & A[12]);
+		C[1] = A[11] ^ ((~A[12]) & A[13]);
+		C[2] = A[12] ^ ((~A[13]) & A[14]);
+		C[3] = A[13] ^ ((~A[14]) & A[10]);
+		C[4] = A[14] ^ ((~A[10]) & A[11]);
+		A[10] = C[0];
+		A[11] = C[1];
+		A[12] = C[2];
+		A[13] = C[3];
+		A[14] = C[4];
 		
-		C[0] = A[index(0, 3)] ^ ((~A[index(0+1, 3)]) & A[index(0+2, 3)]);
-		C[1] = A[index(1, 3)] ^ ((~A[index(1+1, 3)]) & A[index(1+2, 3)]);
-		C[2] = A[index(2, 3)] ^ ((~A[index(2+1, 3)]) & A[index(2+2, 3)]);
-		C[3] = A[index(3, 3)] ^ ((~A[index(3+1, 3)]) & A[index(3+2, 3)]);
-		C[4] = A[index(4, 3)] ^ ((~A[index(4+1, 3)]) & A[index(4+2, 3)]);
-		A[index(0, 3)] = C[0];
-		A[index(1, 3)] = C[1];
-		A[index(2, 3)] = C[2];
-		A[index(3, 3)] = C[3];
-		A[index(4, 3)] = C[4];
+		C[0] = A[15] ^ ((~A[16]) & A[17]);
+		C[1] = A[16] ^ ((~A[17]) & A[18]);
+		C[2] = A[17] ^ ((~A[18]) & A[19]);
+		C[3] = A[18] ^ ((~A[19]) & A[15]);
+		C[4] = A[19] ^ ((~A[15]) & A[16]);
+		A[15] = C[0];
+		A[16] = C[1];
+		A[17] = C[2];
+		A[18] = C[3];
+		A[19] = C[4];
 		
-		C[0] = A[index(0, 4)] ^ ((~A[index(0+1, 4)]) & A[index(0+2, 4)]);
-		C[1] = A[index(1, 4)] ^ ((~A[index(1+1, 4)]) & A[index(1+2, 4)]);
-		C[2] = A[index(2, 4)] ^ ((~A[index(2+1, 4)]) & A[index(2+2, 4)]);
-		C[3] = A[index(3, 4)] ^ ((~A[index(3+1, 4)]) & A[index(3+2, 4)]);
-		C[4] = A[index(4, 4)] ^ ((~A[index(4+1, 4)]) & A[index(4+2, 4)]);
-		A[index(0, 4)] = C[0];
-		A[index(1, 4)] = C[1];
-		A[index(2, 4)] = C[2];
-		A[index(3, 4)] = C[3];
-		A[index(4, 4)] = C[4];
+		C[0] = A[20] ^ ((~A[21]) & A[22]);
+		C[1] = A[21] ^ ((~A[22]) & A[23]);
+		C[2] = A[22] ^ ((~A[23]) & A[24]);
+		C[3] = A[23] ^ ((~A[24]) & A[20]);
+		C[4] = A[24] ^ ((~A[20]) & A[21]);
+		A[20] = C[0];
+		A[21] = C[1];
+		A[22] = C[2];
+		A[23] = C[3];
+		A[24] = C[4];
 		
         // Iota
-		A[0] = A[0] ^ KeccakRoundConstants[round_number];
+		A[0] = A[0] ^ KeccakRoundConstants[x];
     }
     
     for(x = 0; x < WORDS_NUMBER; x++)
@@ -403,8 +430,8 @@ void launch_kernel(unsigned long long *messages_h, unsigned int token_number)
 	}
 
 	// Launch new kernel
-	//kernel_optimixed<<<num_blocks, threads_per_block>>>(buffer_d, state_d);
-	kernel<<<num_blocks, threads_per_block>>>(buffer_d, state_d);
+	kernel_optimixed<<<num_blocks, threads_per_block>>>(buffer_d, state_d);
+	//kernel<<<num_blocks, threads_per_block>>>(buffer_d, state_d);
 }
 
 
